ecba817e5baca57a86754abed7a38300ac0fd4e0
Bogdan Popa <bogdy2p@yahoo.com>
2014-10-22 22:33:31 +0300

    Finally added functionality for the case when only the groups must be updated (if old password input is empty)

--- models/edit_user_model.php
+++ models/edit_user_model.php
@@ -6,130 +6,81 @@ require_once('../controllers/group.php');
 ?>
 
 <?php 	
-verification($_GET);
-//FLOW DIAGRAM//
-//
-//	verificam daca este setat get-ul.
-//	
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-
-function create_user_update_details_array($post_array){
-	$user_update_details = array(
-		'id' => $post_array['id'],
-		'name' => $post_array['name'],
-		'password' => md5($post_array['password']),
-		);
-		return $user_update_details;
-}
 
 
-
-
-
+verification($_GET); //CALL THE VERIFICATION FUNCTION
 
 function verification($get) {
 	
-	if(isset($get) && ($get != NULL)) //CONDITIE ABSOLUT NECESARA PENTRU A ACCESA PAGINA ?!
+	if(isset($get) && ($get != NULL))
 		{ 
-			//$_GET['id'] = $get['id'];	// Remove this ! 		
 			$user = new User(); //Call User Class
 			$user->get_user_object_by_id($get['id']); //Fetch the user object from the database by the ID !!
 			$old_pass = $user->password; // Grab the old password from the object
-//			var_dump($the_old_pass);
-			$old_name = $user->name;
-
-			//Setam Post de id sa corespunda cu get de id.
+			$old_name = $user->name;			
 			$_POST['id'] = $get['id'];
 						
-					//DACA EXISTA POST[OLD PASSWORD]
 					if(isset($_POST['old_password'])) { 
-						//Daca post de old password e cel din database 
-						if(md5($_POST['old_password']) == $old_pass){
-							// daca post de noua parola e egal cu post de confirmare noua parola
+							if(!empty($_POST['old_password'])) {
+									if(md5($_POST['old_password']) == $old_pass){
+											if($_POST['password'] == $_POST['pass_conf']) {
 
-								if($_POST['password'] == $_POST['pass_conf']) {
-
-									//Create the update details array using the post data.																		
-									$user_update_details = create_user_update_details_array($_POST);
-									//Update the user details correspondingly
-									$update = $user->update($user_update_details['id'],'users',$user_update_details);
-									//Delete all the mapping for this user			
-									$delete_current_mapping = $user->delete_all_mapping_for_user($get['id']);
-									
-									//****************************************************************************************//
-
-									//Get an array of checked groups in the form
-									$name_of_groups_checked_in_form = get_groups_checked_in_form();
-									//Create an array of the id's we need to assign !
-									//THIS SHOULD BE A SEPARATE FUNCTION THAT RETURNS THE GROUP_IDS_CHECKED_IN_FORM
-
-									$group_ids_checked_in_form = array();
-									echo 'Untill here';
-									 foreach ($name_of_groups_checked_in_form as $key => $value) {
-									// 	//echo $key;
-									 	$group_ids_checked_in_form[] = $user->get_groupid_by_groupname($value);	
-									 }
-									echo 'Untill here2';
-									$group_ids_checked_array = $group_ids_checked_in_form;
-									//$group_ids_checked_array = array(0,1);
-									print_r($group_ids_checked_array);
-									//**************************************************************************************////
-									//Apply new mapping using the new values from the form !!!!
-									foreach ($group_ids_checked_array as $group_id_checked) {
-										$user->assign_user_to_group($get['id'],$group_id_checked);
+												//Create the update details array using the post data.																		
+												$user_update_details = create_user_update_details_array($_POST);
+												//Update the user details correspondingly
+												$update = $user->update($user_update_details['id'],'users',$user_update_details);
+												//Delete all the mapping for this user			
+												$delete_current_mapping = $user->delete_all_mapping_for_user($get['id']);
+												//Get an array of checked groups in the form
+												$group_ids_checked_array = get_group_ids_checked_in_form();
+												//Apply new mapping using the new values from the form !!!! (Foreach in one line)
+												foreach ($group_ids_checked_array as $group_id_checked) {$user->assign_user_to_group($get['id'],$group_id_checked);}
+												header("Location: /user/views/view_list.php");		
+												die();						
+											}else{
+												print_r("The passwords you entered do not match.");
+											}
+									}else{
+										echo "That is not the current password for this user !";
+										die();
 									}
-									echo 'Untill here3';
-									header("Location: /user/views/view_list.php");		
-									//die();						
-								
-								}else{
-									print_r("The passwords you entered do not match.");
-								}
-						} //if(md5($_POST['old_password']) == $the_old_pass)
-						 else  {
-						 	echo " <br />error. <br />";
-							print_r($old_pass." <= the old pass |||| ".$_POST['old_password']." <= post de old pass <br />");
-							print_r("<br />".$old_pass." <= the old pass |||| ".md5($_POST['old_password'])." <= md5 de post de old pass <br />");
-								}
-					}
-					//DACA NU EXISTA POST[OLD PASSWORD]
-					else{
-						echo "form not submitted yet / post old password not set ";
-					}
-		}else
-		//DACA EXISTA GET-UL (accesare link fara id)
-		{
-			die("There is no get. Or it's NULL // 404 Redirect Here !");
-		}
-} // SFARSIT FUNCTIE verification
-		
-// function get_group_ids_checked_in_form(){
-// 	$name_of_groups_checked_in_form = get_groups_checked_in_form();
-// 	//print_r($name_of_groups_checked_in_form);
-// 	$array_of_group_ids_checked_in_form = array();
-// 		foreach ($name_of_groups_checked_in_form as $key => $value) {
-// 			$array_of_group_ids_checked_in_form[] = $user->get_groupid_by_groupname($value);	
-// 			print_r($value);
-// 		}
-// 	return $array_of_group_ids_checked_in_form;
-// }
+							}	
+							 else  {//If field OLD PASSWORD IS EMPTY
+							 		$delete_current_mapping = $user->delete_all_mapping_for_user($get['id']);
+							 		$group_ids_checked_array = get_group_ids_checked_in_form();
+							 		foreach ($group_ids_checked_array as $group_id_checked) {$user->assign_user_to_group($get['id'],$group_id_checked);}
+							 		header("Location: /user/views/view_list.php");
+								 	die();
+								 	//echo " <br />error. Aici vine functionalitatea in caz ca post de old password e empty. <br />";
+									}
 
+					}else /*$_POST['old_password'] is not set*/{
+						//echo "form not submitted ZZZyet / post old password not set ";
+						//die();
+						//IF $_POST['old_password'] is not set / is empty / is null / is anything but completed!
+						////////////////////////////////////////// FLOW :///////////////////////////////////////
+						// grab user id from the GET
+						// Delete current mapping for this user
+						// Get an array of checked groups in the form
+						// Apply new mapping using the new values from the form
+						// Redirect to view_list.php
+						// Then die();
+					}
+		}else{
+			die("There is no get. Or it's NULL // 404 Redirect Here !");
+			 }
+} //end Verification
+	
+
+function get_group_ids_checked_in_form(){
+	$user = new User();
+	$name_of_groups_array = get_groups_checked_in_form();
+	$array_of_group_ids_checked = array();
+		foreach ($name_of_groups_array as $key => $value) {
+			$array_of_group_ids_checked[] = $user->get_groupid_by_groupname($value);	
+		}
+	return $array_of_group_ids_checked;
+}
 
 function get_groups_checked_in_form(){
 	$user = new User();
@@ -145,6 +96,15 @@ function get_groups_checked_in_form(){
 	return $groups_selected;
 }
 
+
+function create_user_update_details_array($post_array){
+	$user_update_details = array(
+		'id' => $post_array['id'],
+		'name' => $post_array['name'],
+		'password' => md5($post_array['password']),
+		);
+		return $user_update_details;
+}
 
 function print_group_checkboxes_inputs(){
 				$user = new User();
@@ -171,12 +131,13 @@ function print_userdata_inputs(){
 	echo '
 		<label>Name</label><br />
 		<input name="name"  type="text"  placeholder="User Name" value="'.$user->name.'"> <br />
+		<label>Enter current Password</label><br />
+		<input name="old_password"  type="password"  placeholder="Old Password" value=""><br />
 		<label>New Password</label><br />
 		<input name="password"  type="password"  placeholder="New Password" value=""><br />
 		<label>Confirm New Password</label><br />
 		<input name="pass_conf" type="password"  placeholder="Confirm New Password" value=""><br />
-		<label>Enter current Password</label><br />
-		<input name="old_password"  type="password"  placeholder="Old Password" value=""><br />
+		
     ';
 	}
 }
