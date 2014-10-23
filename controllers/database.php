45dd8d5bed318c0b0781d698c405bf26cc5d69fa
Popa Bogdan Constantin <bogdan.popa@reea.net>
2014-10-23 10:10:06 +0300

    Added responsive navigation menu + Fixed include_page_footer_content()

--- controllers/database.php
+++ controllers/database.php
@@ -23,50 +23,54 @@ function include_page_footer_content(){
 	echo '<!-- jQuery (necessary for Bootstrap JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>';
+    <script src="../assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>';
 }
 
 function print_sitewide_menu(){
     echo'   
-            <div class="row">
-
-                <div class="col-xs-2 col-md-2"></div>
-
-                    <div class="col-xs-8 col-md-8 pbc_mt5">
-
-                        <a class ="btn btn-primary" href="/user">Home</a>
-                        <a class ="btn btn-primary" href="/user/views/view_list.php             ">   View Tables         </a>
-                        <a class ="btn btn-primary" href="/user/views/view_group.php            ">   View Group          </a>
-                        <a class ="btn btn-primary" href="/user/views/view_user.php             ">   View User           </a>
-                        <a class ="btn btn-primary" href="/user/views/assign.php                ">   Map User            </a>
-                        <a class ="btn btn-primary" href="/user/views/view_detail_types.php     ">   User Details Types  </a>
-
-                    </div>
-
-                <div class="col-xs-2 col-md-2"></div>
-
-            </div> <!--end of div class row -->
 
             <div class="row">
+                <button type="button" class="btn btn-danger" data-toggle="collapse" data-target="#demo">
+                        simple collapsible
+                </button>
+                <div id="demo" class="collapse in">...</div>
 
-                <div class="col-xs-2 col-md-2"></div>
+             </div>
 
-                    <div class="col-xs-8 col-md-8 pbc_mt5">
+            <nav class="navbar navbar-default" role="navigation">
 
-                        <a class ="btn btn-primary" href="/user/views/create_user.php">Add User</a>
-                        <a class ="btn btn-primary" href="/user/views/create_group.php">Add Group</a>
-                        <!--<a class ="btn btn-primary" href="/user">...</a>
-                        <a class ="btn btn-primary" href="/user">...</a>
-                        <a class ="btn btn-primary" href="/user">...</a>
-                        <a class ="btn btn-primary" href="/user">...</a>
-                        -->
-                        
+                <div class="container-fluid">
+                    <div class="navbar-header">
 
-                    </div>
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>        
 
-                <div class="col-xs-2 col-md-2"></div>
+                            <a class="navbar-brand" href="/user">Home</a>
+                            <a class="navbar-brand" href="/user">Homedsa</a>
+                    </div>                            
 
-            </div> <!--end of div class row below -->
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav">
+                         
+                             <li><a href="/user/views/view_list.php             ">   View Tables         </a></li>
+                             <li><a href="/user/views/view_group.php            ">   View Group          </a></li>
+                             <li><a href="/user/views/view_user.php             ">   View User           </a></li>
+                             <li><a href="/user/views/assign.php                ">   Map User            </a></li>
+                             <li><a href="/user/views/view_detail_types.php     ">   User Details Types  </a></li>
+                             <li><a href="/user/views/create_user.php           ">   Add User            </a></li>
+                             <li><a href="/user/views/create_group.php          ">   Add Group           </a></li>
+
+                        </ul>
+                
+                    </div><!-- /.navbar-collapse -->
+                    
+                </div>
+            </nav>
 ';
 }
 
