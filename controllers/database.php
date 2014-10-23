f45de6abaa430952896227144e88866207696170
Popa Bogdan Constantin <bogdan.popa@reea.net>
2014-10-23 14:28:30 +0300

    Added and tested responsivness for index file

--- controllers/database.php
+++ controllers/database.php
@@ -15,7 +15,10 @@ class Database{
     echo '<link rel="stylesheet" type="text/css" href="assets/css/style.css">';
     echo '<link rel="stylesheet" type="text/css" href="../assets/css/style.css">';
     echo '<link href="../assets/css/bootstrap.min.css" rel="stylesheet">';
-    echo '<link href="assets/css/bootstrap.min.css" rel="stylesheet">'; 
+    echo '<link href="../assets/css/bootstrap-theme.min.css" rel="stylesheet">';
+    echo '<link href="assets/css/bootstrap.min.css" rel="stylesheet">';
+    echo '<link href="assets/css/bootstrap-theme.min.css" rel="stylesheet">';
+    
 }
 
 function include_page_footer_content(){
@@ -73,4 +76,37 @@ function print_error_midscreen($error){
     echo '<div class="col-xs-4 col-md-4"></div>';
     echo '</div>';
     }
+}
+
+
+function print_latest_work_list(){
+    echo '  <ol>
+                    <li><h4><spanred>Added Map Groups to user by "checkbox..."</spanred></h4></li>
+                    <li><h4><spanyel>Fixed map groups by checkbox without changing the user password</spanyel></h4></li>
+                    <li><h4><spanyel>Fixed the bootstrap javascript including (wasn\'t working)</spanyel></h4></li>
+                    <li><h4><spanyel>Removed the old menu ,added a responsive navigation menu </spanyel></h4></li>
+                    <li><h4><spangre>Fixed the site template (The menu and the content have now the same width across all pages)</spangre></h4></li>
+                    <li><h4><spanyel>Implemented print user_details_table & formatted</spanyel></h4></li>
+                    <li><h4><spanred>Implemented Add New Detail Form & Functionality</spanred></h4></li>
+                    <li><h4><spanred>Added options to delete and edit each detail type + Functionality </spanred></h4></li>
+
+                    <li><h4><spangre> Reorganized CRUD</spangre></h4></li>
+                    <li><h4><spanyel> </spanyel></h4></li>
+                    <li><h4><spanyel> </spanyel></h4></li>
+            </ol>
+    ';
+}
+
+function print_to_do_list(){
+    echo'
+                <ol>
+                    <li><h5><spanred>FIX the responsiveness of the website...</spanred></h5></li>
+                    <li><h5><spanred>View User Detail Types Page and Functionality</spanred></h5></li>
+                    <li><h5><spanyel>When creating a new user , save the user\'s account CREATION TIME. (into new table... LOG ?)</spanyel></h5></li>
+                    <li><h5><spanyel>AJAX / Jquery @ editing user </spanyel></h5></li>
+                    <li><h5><spanyel>PRINT DATABASE STATISTICS as : how many programmers, how many users in total , how many designer , how many X.</spanyel></h5>  </li>
+                    <li><h5><spangre>Print "last created user";</spangre></h5></li>
+                    <li><h5><spanyel>Print User with most details entered</spanyel></h5></li>
+                </ol>
+    ';
 }
--- index.php
+++ index.php
@@ -11,77 +11,60 @@ require_once 'controllers/database.php';
 </head>
 
 <body>
-	<div class="container-fluid">
+	<div class="container">
 
-		<div class="row"><?php print_sitewide_menu();?></div>
+			<div class="row"><?php print_sitewide_menu();?></div>
 
-		<div class="row">
-			 <div class="col-xs-4 col-md-4"></div>
-  			 <div class="col-xs-4 col-md-4"><h1>Users-Groups Administration</h1></div>
-  			 <div class="col-xs-4 col-md-4"></div>
-  		</div>
+			<div class="row">
+				 <div class="col-xs-12 col-md-4 "></div>
+	  			 <div class="col-xs-12 col-md-4 "><h1>Users-Groups Administration</h1></div>
+	  			 <div class="col-xs-12 col-md-4 "></div>
+	  		</div>
 
-		<div class="row">
-		<hr>
-			 <div class="col-xs-3 col-md-3">
-			 		<h2>Latest Changes:</h2>
-			 </div>
-			 <div class="col-xs-7 col-md-7">
-			  	
-			  	<ol>
-				  	<li><h4><spanred>Added Map Groups to user by "checkbox..."</spanred></h4></li>
-				  	<li><h4><spanora>Fixed map groups by checkbox without changing the user password</spanora></h4></li>
-				  	<li><h4><spanyel>Fixed the bootstrap javascript including (wasn't working)</spanyel></h4></li>
-				  	<li><h4><spanora>Removed the old menu ,added a responsive navigation menu </spanora></h4></li>
-				  	<li><h4><spangre>Fixed the site template (The menu and the content have now the same width across all pages)</spangre></h4></li>
-			  	</ol>
-			 	
-			 </div>
-			 <div class="col-xs-2 col-md-2"></div>
-	  	
-	  	</div>
-	  	<div class="row">
-	  	<hr>
-		  	<div class="col-xs-3 col-md-3">
-		  		<h2>To do (to change/add):</h2>
+			<div class="row">
+			<hr>
+				 <div class="col-xs-12 col-md-3 ">
+				 	<h2>Latest Changes:</h2>
+				 </div>
+				 <div class="col-xs-12 col-md-7 ">
+				  	<?php print_latest_work_list();?>
+				 </div>
+				 <div class="col-xs-12 col-md-2 "></div>
+		  	
+		  	</div>
+		  	<div class="row">
+		  	<hr>
+			  	<div class="col-xs-12 col-md-3">
+			  		<h2>To do (to change/add):</h2>
+			  	</div>
+
+			  	 <div class="col-xs-12 col-md-6">
+			  	 	<?php print_to_do_list();?>
+				</div>
+				<div class="col-xs-12 col-md-3">
+					///////// ////////// //////?DATABASE STATISTICS Block?/// / // ///// //////////////
+				</div>	
 		  	</div>
 
-		  	 <div class="col-xs-6 col-md-6">
-		  		<ol>
-			  		<li><h5><spanred>FIX the responsiveness of the website...</spanred></h5></li>
-			  		<li><h5><spanred>View User Detail Types Page and Functionality</spanred></h5></li>
-			  		<li><h5><spanyel>When creating a new user , save the user's account CREATION TIME. (into new table... LOG ?)</spanyel></h5></li>
-			  		<li><h5><spanora>AJAX / Jquery @ editing user </spanora></h5></li>
-			  		<li><h5><spanyel>PRINT DATABASE STATISTICS as : how many programmers, how many users in total , how many designer , how many X.</spanyel></h5>	</li>
-			  		<li><h5><spangre>Print "last created user";</spangre></h5></li>
-			  		<li><h5><spanora>Print User with most details entered</spanora></h5></li>
-		  		</ol>
-			</div>
-			<div class="col-xs-3 col-md-3">
-				///////// ////////// //////?DATABASE STATISTICS Block?/// / // ///// //////////////
-			</div>	
-	  	</div>
+		  	<div class="row">
+		  	<hr>
+		  		<div class="col-xs-12 col-md-2"><h1>Colours meaning</h1></div>
+		  		<div class="col-xs-12 col-md-7">
+		  			<ul>
+		  				<li><spanred><h5><b>RED</b></h5></spanred> = HIGH PRIORITY / HIGH DIFFICULTY</li>
+		  				<li><spanyel><h5><b>YELLOW</b></h5></spanyel> = NORMAL PRIORITY / NORMAL DIFFICULTY</li>
+		  				<li><spangre><h5><b>GREEN</b></h5></spangre> = LOW PRIORITY / LOW DIFFICULTY</li>
+		  			</ul>
+		  		</div>
+		  		<div class="col-xs-12 col-md-3"></div>
+		  		 
 
-	  	<div class="row">
-	  	<hr>
-	  		<div class="col-xs-2 col-md-2"><h1>Colours meaning</h1></div>
-	  		<div class="col-xs-7 col-md-7">
-	  			<ul>
-	  				<li><spanred><h5><b>RED</b></h5></spanred> = HIGH PRIORITY / HIGH DIFFICULTY</li>
-	  				<li><spanora><h5><b>ORANGE</b></h5></spanora> = MEDIUM PRIORITY / MEDIUM DIFFICULTY</li>
-	  				<li><spanyel><h5><b>YELLOW</b></h5></spanyel> = NORMAL PRIORITY / NORMAL DIFFICULTY</li>
-	  				<li><spangre><h5><b>GREEN</b></h5></spangre> = LOW PRIORITY / LOW DIFFICULTY</li>
-	  			</ul>
-	  		</div>
-	  		<div class="col-xs-3 col-md-3"></div>
-	  		 
-
-	  	</div>
+		  	</div>
 
 
-	  	<br />
-				  	
-				  	
+		  	<br />
+					  	
+					  	
 				  	
 				  	
 				  	
