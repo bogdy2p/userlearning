8e1e711147eed709b9bc7726ce04b7a25a386478
Popa Bogdan <bogdy2p@yahoo.com>
2014-10-25 13:06:16 +0300

    Created 2 new tables in the db. app_changelog & app_changelog_colours . Created a new controller class named Changelog . Starting implementation of new controller class

---
+++ controllers/changelog_controller.php
@@ -0,0 +1,41 @@
+<?php
+
+class Changelog extends Crud { 
+	//Constructor function for class Changelog.
+	function __construct(){
+		parent::__construct('changelog');
+	}
+	 function create($array,$table = 'app_changelog'){
+		return parent::create($array,$table);
+	}
+	function read($table_name = 'app_changelog'){
+		return parent::read($table_name);
+	}
+	
+	// function list_changelog($table_name = 'app_changelog'){
+	// 	return parent::read($table_name);
+	// }
+
+	// function list_changelog_bydate_desc($table_name = 'app_changelog'){
+	// 	$this->db = new Database();
+	// 	$this->db = $this->db->dbConnect();
+	//  	$statement = $this->db->prepare("SELECT * FROM app_changelog ORDER BY date_created DESC");
+	//  	$statement->execute();
+	//  	//print_r($statement);
+	//  	return $statement;
+	// }
+
+
+
+	// function create_changelog_row($name,$message){
+	// 	$this->db = new Database();
+	// 	$this->db = $this->db->dbConnect();
+	// 	$name = $name;
+	// 	$date_created = time();
+	// 	$statement = $this->db->prepare("INSERT INTO app_changelog (name,text,date_created) VALUES (?,?,NOW())");
+	// 	$statement->bindParam(1,$name);
+	// 	$statement->bindParam(2,$message);
+	// 	$statement->execute();
+	// }
+}
+?>
